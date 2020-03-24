a401c439ebd4c27a6b08c95a6424ad93ac2b5b18
ChanHeeGit <l01083582454@gmail.com>
2020-03-24 14:45:39 +0900

    스프링 DI, xml configuration 복습

---
+++ DI_basic/src/spring/di/ui/GridExamConsole.java
@@ -0,0 +1,29 @@
+package spring.di.ui;
+
+import spring.di.entity.Exam;
+
+public class GridExamConsole implements ExamConsole {
+
+	private Exam exam;
+	
+	public GridExamConsole() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public GridExamConsole(Exam exam) {
+		this.exam = exam;
+	}
+	
+	@Override
+	public void print() {
+		System.out.println("그리드 Exam입니다.");
+
+	}
+
+	@Override
+	public void setExam(Exam exam) {
+		this.exam=exam;
+		
+	}
+
+}
---
+++ DI_basic/src/spring/di/ui/InlineExamConsole.java
@@ -0,0 +1,33 @@
+package spring.di.ui;
+
+import spring.di.entity.Exam;
+
+public class InlineExamConsole implements ExamConsole {
+
+	private Exam exam;
+	
+	//기본생성자
+	public InlineExamConsole() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	//생성자
+	public InlineExamConsole(Exam exam) {
+		this.exam = exam;
+	}
+
+
+
+	@Override
+	public void print() {
+		System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
+
+	}
+	
+	@Override
+	public void setExam(Exam exam) {
+		this.exam=exam;
+		
+	}
+
+}
