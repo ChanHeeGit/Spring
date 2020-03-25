28159f0d3e4a3dea0a81a7475f0849094a083e5f
ChanHeeGit <l01083582454@gmail.com>
2020-03-25 20:21:25 +0900

    생성자를 통한 di

--- DI_basic/src/spring/di/entity/NewlecExam.java
+++ DI_basic/src/spring/di/entity/NewlecExam.java
@@ -7,7 +7,19 @@ public class NewlecExam implements Exam {
 	private int math;
 	private int com;
 	
+	public NewlecExam() {
+		// TODO Auto-generated constructor stub
+	}
 	
+	
+	public NewlecExam(int kor, int eng, int math, int com) {
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.com = com;
+	}
+
+
 	@Override
 	public int total() {
 		// TODO Auto-generated method stub
